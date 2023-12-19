872a8be3aa4b450c241e39497389e042a188eeb1
xyhlzs <891466233@qq.com>
2023-12-19 14:02:33 +0800

    spring build

---
+++ src/main/java/com/internship/internshipweb/InternshipWebApplication.java
@@ -0,0 +1,13 @@
+package com.internship.internshipweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internship/internshipweb/InternshipWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.internshipweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
